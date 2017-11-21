154602c08eea160a5b65bcd124e1a4bb1fdffb89
BonizLee <bonizlee@outlook.com>
2017-11-22 01:26:29 +0800

    完成GUI程序

--- MarkStatisticsW.py
+++ MarkStatisticsW.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-
 """
 statistics the score GUI
 file:MarkStatisticsW.py
@@ -9,41 +8,46 @@ author:BonizLee
 """
 import json
 import sys
-from PyQt5.QtWidgets import QMainWindow, QApplication, QAction, QTextEdit
+from PyQt5.QtWidgets import QMainWindow, QApplication, QAction, QTextEdit, QMessageBox
 from PyQt5.QtGui import QIcon
 from openpyxl import Workbook
 from openpyxl import load_workbook
 from openpyxl.utils import column_index_from_string
 import os.path
 
-class MarkStatisticsW(QMainWindow):   
+class MarkStatisticsW(QMainWindow):  
 
     def __init__(self):
-        super().__init__()      
+        super().__init__()
         self.initUI()
         self.initConfig()
-
-    textMessage = QTextEdit()
+         
     def initUI(self):
         self.setWindowTitle('自动分数统计')
-        self.setGeometry(300,300,500,400)
-        self.setWindowIcon(QIcon('ms.ico'))        
+        self.setGeometry(300, 300, 500, 400)
+        self.setWindowIcon(QIcon('ms.ico'))
         self.setStatusBar('加载配置完成')
         
-        calcAction = QAction(QIcon('calc.png'),'点击计算',self)
-        calcAction.setStatusTip('点击进行计算')
-        calcAction.triggered.connect(self.calc)
+        self.calcAction = QAction(QIcon('calc.png'), '点击计算', self)
+        self.calcAction.setStatusTip('点击进行计算')
+        self.calcAction.triggered.connect(self.calc)
         self.toolbar = self.addToolBar('点击计算')
-        self.toolbar.addAction(calcAction)
+        self.toolbar.addAction(self.calcAction)
 
         self.textMessage = QTextEdit()
-        self.textMessage.setReadOnly(True)     
+        self.textMessage.setReadOnly(True)
+        self.setCentralWidget(self.textMessage)
         self.show()
         
     # 设置状态栏
-    def setStatusBar(self,message):
+    def setStatusBar(self, message):
         self.statusBar().showMessage(message)
     
+
+    def errorDialog(self, msg):
+        reply = QMessageBox.warning(self, "错误", msg, QMessageBox.Ok)
+        
+
     # 计算
     def calc(self):
         self.summary()
@@ -55,10 +59,21 @@ class MarkStatisticsW(QMainWindow):
     def initConfig(self):   
         global PATH
         PATH = os.path.dirname(os.path.realpath(__file__))+os.path.sep
-        fp = open(PATH+'config.json')    
+        try:
+            fp = open(PATH+'config.json') 
+        except IOError:
+            self.errorDialog('打开配置文件错误')            
+            QCoreApplication.quit()
+
         global COMMOM_DATA
-        COMMOM_DATA = json.load(fp)
-        #print(COMMOM_DATA)
+        try:
+            COMMOM_DATA = json.load(fp)
+        except Exception:
+            self.errorDialog('读取配置错误')            
+            QCoreApplication.quit()
+        
+        self.textMessage.append('加载配置文件内容:')
+        self.textMessage.append(str(COMMOM_DATA))
         global MAXNUMBER 
         MAXNUMBER = COMMOM_DATA['maxnumber']
         global PROJECT 
@@ -75,97 +90,95 @@ class MarkStatisticsW(QMainWindow):
             subjectmark =  [ [ 0 for i in range(MAXNUMBER) ] for j in range(judges) ]
             for n in range(0,judges):            
                 filename = PATH+PROJECT+subject['filename']+str(n+1)+FILETYPE
-                workbook = load_workbook(filename,data_only=True)
-                sheetnames =workbook.get_sheet_names()
-                sheet = workbook.get_sheet_by_name(sheetnames[0])
+                try:
+                    workbook = load_workbook(filename,data_only=True)
+                    sheetnames =workbook.get_sheet_names()
+                    sheet = workbook.get_sheet_by_name(sheetnames[0])
+                except Exception:
+                    self.errorDialog('读取'+filename+'错误')
+                    QCoreApplication.quit()
+
                 start_cell = sheet[subject['markcell']]
                 start_col = column_index_from_string(start_cell.column)
                 start_row = start_cell.row
-                for i in range(0,MAXNUMBER):
-                    markcell = sheet.cell(row=start_row,column=start_col+i)                
+                for i in range(0, MAXNUMBER):
+                    markcell = sheet.cell(row=start_row, column=start_col+i)               
                     subjectmark[n][i] = markcell.value
-            print('评分表'+subject['filename']+'完成')
-            print(subjectmark)
+            self.textMessage.append('评分表'+subject['filename']+'完成')
+            self.textMessage.append(str(subjectmark))
             calc_type = subject['calculate']
             if calc_type == 1:
-                self.average(subjectmark,judges)
+                self.average(subjectmark, judges)
             elif calc_type == 2:
-                self.without_max_min_average(subjectmark,judges)
+                self.without_max_min_average(subjectmark, judges)
             elif calc_type == 3:
-                self.without_abs_max_average(subjectmark,judges)     
+                self.without_abs_max_average(subjectmark, judges)
 
     #算术平均
-    def average( self,subjectmark,judges ):    
-        for i in range(0,MAXNUMBER):
+    def average(self, subjectmark, judges):
+        for i in range(0, MAXNUMBER):
             sum = 0
-            for n in range(0,judges):
+            for n in range(0, judges):
                 sum += subjectmark[n][i]
             STUDENT_MARK[i] += sum / judges
 
     #去除最高和最低再求平均
-    def without_max_min_average(  self,subjectmark,judges ):
-        for i in range(0,MAXNUMBER):
+    def without_max_min_average(self, subjectmark, judges):
+        for i in range(0, MAXNUMBER):
             max = subjectmark[0][i]
             sum = subjectmark[0][i]
             min = subjectmark[0][i]
-            for n in range(1,judges):
+            for n in range(1, judges):
                 value = subjectmark[n][i]
                 sum += value
                 if value > max:
                     max = value
                 elif value < min:
-                    min = value       
+                    min = value
             STUDENT_MARK[i] += (sum - min -max) / (judges -2)
 
     #去除偏差最大的再求平均
-    def without_abs_max_average( self,subjectmark,judges ):    
-        for i in range(0,MAXNUMBER):
+    def without_abs_max_average(self, subjectmark, judges):
+        for i in range(0, MAXNUMBER):
             jm = [0 for k in range(judges)]
-            for n in range(0,judges):
+            for n in range(0, judges):
                 jm[n] = subjectmark[n][i]
-            average_mark = sum(jm) / len(jm)               
+            average_mark = sum(jm) / len(jm)
             max_abs = abs(average_mark-jm[0])
             max_mark = jm[0]
-            for j in range(1,judges):
+            for j in range(1, judges):
                 if max_abs < abs(average_mark-jm[j]) and max_mark > jm[j]:
                     max_abs = abs(average_mark-jm[j])
                     max_mark = jm[j]
             STUDENT_MARK[i] += (sum(jm) - max_mark)/(judges - 1)
-        
+
     #写入excel文件
     def write_excel( self):
         filename = PATH+PROJECT + 'summary'+FILETYPE   
         workbook = Workbook()
         sheet = workbook.get_active_sheet()
-        sheet.cell(row=1,column=1).value = '工位号'
-        sheet.cell(row=1,column=2).value = '成绩'
-        sheet.cell(row=1,column=3).value = '名次'
-        first_mark_cell = sheet.cell(row=2,column=2)
-        last_mark_cell = sheet.cell(row=len(STUDENT_MARK)+1,column=2)
+        sheet.cell(row=1, column=1).value = '工位号'
+        sheet.cell(row=1, column=2).value = '成绩'
+        sheet.cell(row=1, column=3).value = '名次'
+        first_mark_cell = sheet.cell(row=2, column=2)
+        last_mark_cell = sheet.cell(row=len(STUDENT_MARK)+1, column=2)
+        self.textMessage.append('成绩:')
+        self.textMessage.append(str(STUDENT_MARK))
+
         for i in range(len(STUDENT_MARK)):
-            sheet.cell(row=2+i,column=1).value = i+1
-            sheet.cell(row=2+i,column=2).number_format = '0.0'
-            sheet.cell(row=2+i,column=2).value = STUDENT_MARK[i]
-            formual_str = '=rank('+sheet.cell(row=2+i,column=2).coordinate + ',' +\
+            sheet.cell(row=2+i, column=1).value = i+1
+            sheet.cell(row=2+i, column=2).number_format = '0.0'
+            sheet.cell(row=2+i, column=2).value = STUDENT_MARK[i]
+            formual_str = '=rank('+sheet.cell(row=2+i, column=2).coordinate + ',' +\
             first_mark_cell.column + '$2:' +\
             last_mark_cell.column +'$'+str(last_mark_cell.row)+ ')'
-            sheet.cell(row=2+i,column=3).value = formual_str
-        workbook.save(filename)
+            sheet.cell(row=2+i, column=3).value = formual_str
+        try:
+            workbook.save(filename)
+        except Exception:
+            self.errorDialog('写入Excel错误')        
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     msw = MarkStatisticsW()
     sys.exit(app.exec_())
-    """
-    init()
-    print('加载配置完成')
-    summary()
-    print('汇总完成完成')
-    print(STUDENT_MARK)
-    write_excel()
-    print('保存完成')
-    while True:
-        q = input("输入Q退出程序：")
-        if q == 'Q' or q == 'q':
-            break 
-    """
